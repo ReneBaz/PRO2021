e0794d1ec9fcead55e149fd8375e048d3fc45602
Rene Bažato <rene.bazato@gmail.com>
2021-10-08 13:27:44 +0200

    FraktaliZacetek

---
+++ Fraktali/Fraktali/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fraktali
+{
+    public partial class Form1 : Form
+    {
+        private double x;
+        private double y;
+        private double alpha;//kot glede na os x
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private int risaniX(double xr)
+        {
+            return (int)Math.Round(this.Width * xr);
+        }
+
+        private int risaniY(double yr)
+        {
+            return (int)Math.Round(this.Height-this.Height * yr);
+        }
+
+        public void obratlevo(double kot)
+        {
+            alpha = alpha + kot;
+        }
+
+        public void premik(double d, Graphics g)
+        {
+            double starix = x;
+            double stariy = y;
+            x = x + Math.Cos(alpha*Math.PI/180);
+            y = y + Math.Sin(alpha * Math.PI / 180);
+            int x1 = risaniX(starix);
+            int y1 = risaniY(stariy);
+            int x2 = risaniX(x);
+            int y2 = risaniY(y);
+            g.DrawLine(new Pen(Color.Black, 3), x1,y1,x2,y2);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            x = 0.5;
+            y = 0.5;
+            alpha = 0;
+            premik(0.5, g);
+        }
+    }
+}
---
+++ HanoiskiStolpi/HanoiskiStolpi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiskiStolpi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            palindrom("rene");
+            Console.Read();
+
+        }
+
+        static void Premakni(int n, string začetni, string končni, string pomožni)
+        {
+            if (n==1)
+            {
+                Console.WriteLine("premakni iz "+začetni+" na "+končni);
+                return;
+            }
+            Premakni(n - 1, začetni, pomožni, končni);//iz a na b pomagaj si s c
+            Console.WriteLine("premakni iz " + začetni + " na "+končni);
+            Premakni(n - 1, pomožni, končni, začetni);
+        }
+
+        static void palindrom(string beseda)
+        {
+            if(beseda.Length ==1 || beseda.Length == 0)
+            {
+                 Console.WriteLine("je palindrom");
+                return;
+            }
+            if (beseda[0].Equals(beseda[beseda.Length-1]))
+            {
+                palindrom(beseda.Substring(1,beseda.Length-2));
+            }
+            else
+            {
+                Console.WriteLine("ni palindrom");
+                return;
+            }
+            
+        }
+    }
+}
