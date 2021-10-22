5dca25e5b6d50393277de866bfa7eb78e49e543d
Rene Bažato <rene.bazato@gmail.com>
2021-10-22 12:25:56 +0200

    Pravokotnik(razred konstruktorji in se vec)POMEMBNO!!!

---
+++ VajaPravokotnik/VajaPravokotnik/Pravokotnik.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaPravokotnik
+{
+    class Pravokotnik
+    {
+        
+        public double Širina { get; set; }
+        public double Višina { get; set; }
+        public Pravokotnik()
+        {
+
+        }
+        public Pravokotnik(double š, double v)
+        {
+            Širina = š;
+            Višina = v;
+        }
+        public double Ploscina()
+        {
+            return Širina * Višina;
+        }
+
+        public static bool operator ==(Pravokotnik p1,  Pravokotnik p2)
+        {
+            return p1.Ploscina() == p2.Ploscina();
+        }
+        public static bool operator !=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return !(p1 == p2);
+        }
+        public static bool operator >(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploscina()> p2.Ploscina();
+        }
+        public static bool operator <(Pravokotnik p1, Pravokotnik p2)
+        {
+            return !(p1 > p2);
+        }
+        public static bool operator >=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploscina() >= p2.Ploscina();
+        }
+        public static bool operator <=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return !(p1 >= p2);
+        }
+
+        public override string ToString()
+        {
+            return "Višina ="+Višina+" Širina ="+Širina ;
+        }
+
+    }
+}
