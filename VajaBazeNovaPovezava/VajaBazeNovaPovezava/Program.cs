cce961bcd8f63dd00ee5dbeb83f7684b551b9084
Rene Bažato <rene.bazato@gmail.com>
2021-12-10 13:26:47 +0100

    sql vaje za izpit

---
+++ VajaBazeNovaPovezava/VajaBazeNovaPovezava/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaBazeNovaPovezava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BazaZaVajeEntities db = new BazaZaVajeEntities();
+            DateTime datum = DateTime.Parse("20.1.2004");
+            var x1 = from a in db.PRODUKT
+                     where a.P_DATUM < datum
+                     select new {a.P_OPIS,a.P_ZALOGA,a.P_MIN,a.P_CENA,a.P_DATUM };
+            foreach(var y in x1)
+            {
+                Console.WriteLine("opis "+y.P_OPIS+"| "+y.P_DATUM);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+            TimeSpan ts = new TimeSpan(365,0,0,0);
+            DateTime zapadlost = DateTime.Now.Add(ts);
+            var x2 = from a in db.PRODUKT
+                     select a;
+            foreach (var y in x2)
+            {
+                Console.WriteLine(y.P_OPIS+"| "+y.P_ZALOGA+"| "+y.P_DATUM+"| "+zapadlost);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+            //izberi P_OPIS, P_ZALOGA, P_MIN,P_CENA iz tabele PRODUKT, kjer je P_CENA manjša od 50 in
+            //je P_DATUM večji kot 15.jan. 2004
+            DateTime datum1 = DateTime.Parse("15.1.2004");
+            var x3 = from a in db.PRODUKT
+                     where a.P_CENA<50 && a.P_DATUM>datum1
+                     select a;
+            foreach (var y in x3)
+            {
+                Console.WriteLine(y.P_OPIS + "| " + y.P_ZALOGA +"| "+y.P_MIN+"| " + y.P_CENA);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+
+            //izberi vse atribute iz tabele DOBAVITELJ katerih ime se začne s Smith
+
+            var x4 = from a in db.DOBAVITELJ
+                     where a.D_IME.StartsWith("Smith")
+                     select a;
+            foreach (var y in x4)
+            {
+                Console.WriteLine(y.D_IME);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+            //izberi vse dobavitelje, kjer je zaloga v produktu manjša od dvakratnika minimalne zaloge
+
+            var x5 = from a in db.PRODUKT
+                     from b in db.DOBAVITELJ
+                     where a.P_ZALOGA<(a.P_MIN*2)
+                     select b;
+            foreach (var y in x5)
+            {
+                Console.WriteLine(y.D_IME);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+            //izberi D_KODA od dobaviteljev, ki so nam že dobavili katerega izmed produktov. Vsak
+            //dobavitelj naj bo v rešitvi samo enkrat
+
+            var x6 = (from a in db.PRODUKT
+                     from b in db.DOBAVITELJ
+                     where a.D_KODA==b.D_KODA
+                     select b).Distinct();
+            foreach (var y in x6)
+            {
+                Console.WriteLine(y.D_IME);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+            //g. izberi kodo in ime dobavitelja, ki nam še niso ničesar dobavili (njihova koda se ne pojavlja v
+            //   tabeli PRODUKT) 
+
+            var x7 = (from a in db.PRODUKT
+                      from b in db.DOBAVITELJ
+                      where b.D_KODA != a.D_KODA
+                      select b).Distinct();
+            foreach (var y in x7)
+            {
+                Console.WriteLine(y.D_IME);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+            //h. izpiši vsoto vseh stanj pri kupcih (KUP_STANJE) (2089,28)
+            
+            var x8 = (from a in db.KUPEC
+                      select a.KUP_STANJE).Sum();
+            Console.WriteLine(x8);
+            Console.WriteLine("----------------------------------------------------");
+
+            //izračunaj celotno vrednost blaga na zalogi (15.084,52€) 
+            var x9 = (from a in db.PRODUKT
+                      select a.P_ZALOGA*a.P_CENA).Sum();
+            Console.WriteLine(x9);
+            Console.WriteLine("----------------------------------------------------");
+
+            //izpiši število različnih produktov posameznega dobavitelja po posameznem dobavitelju iz
+            //tabele produkt(rešitev ima 6 vrstic, če izključimo dobavitelja null) 
+            var x10 = (from a in db.PRODUKT
+                       group a.P_KODA by a.D_KODA into b
+                      select b);
+            foreach (var y in x10)
+            {
+                Console.WriteLine(y.Key);
+            }
+            Console.WriteLine("----------------------------------------------------");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VajaElektrika/VajaElektrika/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaElektrika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ElektrikaEntities db = new ElektrikaEntities();
+            //1. izberi čas meritve in skupno moč
+            var x1 = from a in db.Meritve
+                     select new { Čas = a.ZapisČas, Moč = a.kW1 + a.kW2 + a.kW3 };
+            Console.WriteLine(x1.Count());
+            Console.WriteLine("-----------------------------------------------------------");
+            //ista poizvedba z lambda izrazom
+
+
+            //2. izberi čas meritve in skupno moč za datum 18.8.2013
+            DateTime datum1 = DateTime.Parse("19.8.2013");
+            DateTime datum2 = DateTime.Parse("17.8.2013");
+            var x2 = from a in db.Meritve
+                     where a.ZapisČas<datum1 && a.ZapisČas>datum2
+                     select new { Čas = a.ZapisČas, Moč = a.kW1 + a.kW2 + a.kW3 };
+            //foreach (var y in x2)
+            //{
+            //    Console.WriteLine(y.Čas + "| " + y.Moč);
+            //}
+            Console.WriteLine("-----------------------------------------------------------");
+            //3. izračunaj povprečno moč za datum 18.8.2013
+            var x3 = (from a in db.Meritve
+                     where a.ZapisČas < datum1 && a.ZapisČas > datum2
+                     select a.kW1+a.kW2+a.kW3).Average();
+            Console.WriteLine("Avg "+x3);
+            Console.WriteLine("-----------------------------------------------------------");
+            //4. izračunaj maximalno moč za ta datum
+            var x4 = (from a in db.Meritve
+                      where a.ZapisČas < datum1 && a.ZapisČas > datum2
+                      select a.kW1 + a.kW2 + a.kW3).Max();
+            Console.WriteLine("Max "+x4);
+            Console.WriteLine("-----------------------------------------------------------");
+
+            //5. izračunaj minimalno moč za ta datum
+            var x5 = (from a in db.Meritve
+                      where a.ZapisČas < datum1 && a.ZapisČas > datum2
+                      select a.kW1 + a.kW2 + a.kW3).Min();
+            Console.WriteLine("Min "+x5);
+            Console.WriteLine("-----------------------------------------------------------");
+
+            //6. izračunaj povprečno moč po urah za dan 18.8.2013
+            var x6 = (from a in db.Meritve
+                      where a.ZapisČas < datum1 && a.ZapisČas > datum2
+                      group a.kW1 + a.kW2 + a.kW3 by a.ZapisČas.Value.Hour into b
+                      select b);
+            
+            foreach(var y in x6)
+            {
+                Console.WriteLine("Avg po urah "+y.Key+" " + y.Average());
+            }
+            Console.WriteLine("-----------------------------------------------------------");
+
+            //7.izračunaj 15 minutna povprečja za 18.8.2013
+            var x7 = (from a in db.Meritve
+                      where a.ZapisČas < datum1 && a.ZapisČas > datum2
+                      let ura=a.ZapisČas.Value.Hour
+                      let min=a.ZapisČas.Value.Minute
+                      let quarter=min/15
+                      group a.kW1 + a.kW2 + a.kW3 by new {ura,quarter} into b
+                      select b);
+
+            foreach (var y in x7)
+            {
+                Console.WriteLine("Avg po 15 min "+y.Key + y.Average());
+            }
+            Console.WriteLine("-----------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
