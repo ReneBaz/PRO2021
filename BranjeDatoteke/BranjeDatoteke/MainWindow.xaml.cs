3504c211dcb1aea164a2037851d939e9f2c3a0de
Rene Bažato <rene.bazato@gmail.com>
2021-12-15 09:55:23 +0100

    nek obup k nimam najmansega pojma kaj delam

---
+++ BranjeDatoteke/BranjeDatoteke/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace BranjeDatoteke
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Podatki> vsiPodatki = new List<Podatki>();
+        CollectionViewSource podatkiNewSorce;
+        public MainWindow()
+        {
+            InitializeComponent();
+            podatkiNewSorce = (CollectionViewSource)(FindResource("podatkiViewSorce"));
+            StreamReader sr = new StreamReader(@"d:\PRO2021\vaja.csv");
+            string vrstica = sr.ReadLine();
+            vrstica = sr.ReadLine();
+            while (vrstica != null)
+            {
+                string[] p = vrstica.Split(';');
+                Podatki novi = new Podatki();
+                novi.Id = int.Parse(p[0]);
+                novi.Datum = DateTime.Parse(p[1]);
+                novi.Ime = p[2];
+                novi.Znesek = double.Parse(p[3]);
+                vsiPodatki.Add(novi);
+                vrstica = sr.ReadLine();
+            }
+            sr.Close();
+            DataContext = this;
+            podatkiNewSorce.Source = vsiPodatki;
+        }
+    }
+}
---
+++ WpfinEf/WpfinEf/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfinEf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        northwndEntities1 context = new northwndEntities1();
+        CollectionViewSource custViewSource;
+        CollectionViewSource custumerOrderrsViewSorce;
+        public MainWindow()
+        {
+            InitializeComponent();
+            custViewSource = (CollectionViewSource)(FindResource("customersViewSource"));
+            custumerOrderrsViewSorce = (CollectionViewSource)(FindResource("customersOrdersViewSource"));
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource customersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customersViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // customersViewSource.Source = [generic data source]
+        }
+    }
+}
